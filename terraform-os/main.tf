8bcbf9e9f9025e9ba032ee53ae06ab7f445fa4c4
unknown <rcoward@SED00187.internal.local>
2016-06-20 12:47:00 +0100

    Added more security groups

--- terraform-os/main.tf
+++ terraform-os/main.tf
@@ -1,64 +1,163 @@
 # Configure the OpenStack Provider
 provider "openstack" {
-    user_name  = "${var.OS_USERNAME}"
+    user_name   = "${var.OS_USERNAME}"
     tenant_name = "${var.OS_TENANT_NAME}"
-    password  = "${var.OS_PASSWORD}"
-    auth_url  = "${var.OS_AUTH_URL}"
-    insecure  = "true"
+    password    = "${var.OS_PASSWORD}"
+    auth_url    = "${var.OS_AUTH_URL}"
+    insecure    = "true"
 }
 
-resource "openstack_networking_router_v2" "router_1" {
+resource "openstack_networking_router_v2" "internet_gw" {
   region = ""
-  name = "my_router"
+  name   = "InternetGW"
   external_gateway = "${var.OS_INTERNET_GATEWAY_ID}"
 }
 
-resource "openstack_networking_network_v2" "network_1" {
-  name = "network_1"
+resource "openstack_networking_network_v2" "dmz" {
+  name = "DMZ"
   admin_state_up = "true"
 }
 
-resource "openstack_networking_subnet_v2" "subnet_1" {
-  name = "subnet_1"
-  network_id = "${openstack_networking_network_v2.network_1.id}"
-  cidr = "192.168.199.0/24"
+resource "openstack_networking_subnet_v2" "dmz_subnet" {
+  name       = "dmz_network"
+  network_id = "${openstack_networking_network_v2.dmz.id}"
+  cidr       = "${var.DMZ_Subnet}"
   ip_version = 4
   enable_dhcp = "true"
-  allocation_pools = { start = "192.168.199.50"
-                       end = "192.168.199.200" } 
+  allocation_pools = { start = "${cidrhost(var.DMZ_Subnet, 50)}"
+                       end = "${cidrhost(var.DMZ_Subnet, 200)}" } 
   dns_nameservers  = [ "8.8.8.8" ]
 }
 
-resource "openstack_networking_router_interface_v2" "router_interface_1" {
+resource "openstack_networking_router_interface_v2" "gw_if_1" {
   region = ""
-  router_id = "${openstack_networking_router_v2.router_1.id}"
-  subnet_id = "${openstack_networking_subnet_v2.subnet_1.id}"
+  router_id = "${openstack_networking_router_v2.internet_gw.id}"
+  subnet_id = "${openstack_networking_subnet_v2.dmz_subnet.id}"
 }
 
-resource "openstack_networking_secgroup_v2" "secgroup_1" {
-  name = "secgroup_1"
-  description = "My neutron security group"
+resource "openstack_networking_secgroup_v2" "any_ssh" {
+  name = "External SSH Access"
+  description = "Allow SSH access to VMs"
 }
 
-resource "openstack_networking_secgroup_rule_v2" "secgroup_rule_1" {
+resource "openstack_networking_secgroup_rule_v2" "any_ssh_rule_1" {
   direction = "ingress"
   ethertype = "IPv4"
   protocol = "tcp"
   port_range_min = 22
   port_range_max = 22
   remote_ip_prefix = "0.0.0.0/0"
-  security_group_id = "${openstack_networking_secgroup_v2.secgroup_1.id}"
+  security_group_id = "${openstack_networking_secgroup_v2.any_ssh.id}"
 }
 
-resource "openstack_compute_servergroup_v2" "test-sg" {
-  name = "my-sg"
-  policies = ["anti-affinity"]
-  # policies = ["affinity"]
+resource "openstack_networking_secgroup_v2" "local_ssh" {
+  name = "Local SSH Access"
+  description = "Allow local SSH access to VMs"
 }
+
+resource "openstack_networking_secgroup_rule_v2" "local_ssh_rule_1" {
+  direction = "ingress"
+  ethertype = "IPv4"
+  protocol = "tcp"
+  port_range_min = 22
+  port_range_max = 22
+  remote_ip_prefix = "${var.DMZ_Subnet}"
+  security_group_id = "${openstack_networking_secgroup_v2.local_ssh.id}"
+}
+
+resource "openstack_networking_secgroup_v2" "any_http" {
+  name = "External HTTP Access"
+  description = "Allow HTTP access to VMs"
+}
+
+resource "openstack_networking_secgroup_rule_v2" "any_http_rule_1" {
+  direction = "ingress"
+  ethertype = "IPv4"
+  protocol = "tcp"
+  port_range_min = 80
+  port_range_max = 80
+  remote_ip_prefix = "0.0.0.0/0"
+  security_group_id = "${openstack_networking_secgroup_v2.any_http.id}"
+}
+
+resource "openstack_networking_secgroup_v2" "local_http" {
+  name = "Internal HTTP Access"
+  description = "Allow HTTP access from local VMs"
+}
+
+resource "openstack_networking_secgroup_rule_v2" "local_http_rule_1" {
+  direction = "ingress"
+  ethertype = "IPv4"
+  protocol = "tcp"
+  port_range_min = 80
+  port_range_max = 80
+  remote_ip_prefix = "${var.DMZ_Subnet}"
+  security_group_id = "${openstack_networking_secgroup_v2.local_http.id}"
+}
+
+resource "openstack_networking_secgroup_v2" "local_mysql" {
+  name = "Internal MySQL Access"
+  description = "Allow MySQL access from local VMs"
+}
+
+resource "openstack_networking_secgroup_rule_v2" "local_mysql_rule_1" {
+  direction = "ingress"
+  ethertype = "IPv4"
+  protocol = "tcp"
+  port_range_min = 3306
+  port_range_max = 3306
+  remote_ip_prefix = "${var.DMZ_Subnet}"
+  security_group_id = "${openstack_networking_secgroup_v2.local_mysql.id}"
+}
+
 
 resource "openstack_compute_keypair_v2" "ssh-keypair" {
-  name = "terraform-keypair"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEArrSmhh3oGC/zCorMpOXXppYCRLQMzUpdyQotCuXQ8uD4JHitbReQBT0LuaCtei4xxLcdnpNW7DM/xLfl0yZLWRk0iH5VFjPxvYsgVzhysO8nvG6l2p3RDJOrA2mQROl5yDaAWUE/J2vKezzNJf9f8/JZyuWWafPa89+XTPR3VAuQCzRrCmskW1rpok29IpqM/jM7QCfG20Y7/lLOaCW9UlEAO++WTDQS4X6t6Xf5Lsg6TMGrMuPSQzjUTMGvfhT3dnTuhALLW5bYQirkgAWwSPoHx5yeZZnhH1C0T2ak0Dhx0tlm9sr82DU6x0INWENcdMj95oFlceA5DOqQ/3s3jw=="
+  name       = "terraform-keypair"
+  public_key = "${var.SSH_KEY}"
+}
+
+resource "openstack_compute_floatingip_v2" "jumpbox_ip" {
+  region = ""
+  pool = "Internet"
+}
+
+resource "openstack_compute_floatingip_v2" "loadbalancer_ip" {
+  region = ""
+  pool = "Internet"
+}
+
+resource "openstack_compute_instance_v2" "jumpbox" {
+  name        = "jumpbox01"
+  image_name  = "${var.IMAGE_NAME}"
+  flavor_name = "${var.INSTANCE_TYPE}"
+  key_pair    = "${openstack_compute_keypair_v2.ssh-keypair.name}"
+  security_groups = ["${openstack_networking_secgroup_v2.any_ssh.name}"]
+
+  network {
+    name = "${openstack_networking_network_v2.dmz.name}"
+    fixed_ip_v4 = "${cidrhost(var.DMZ_Subnet, 5)}"
+    floating_ip = "${openstack_compute_floatingip_v2.jumpbox_ip.address}"
+  }
+}
+
+resource "openstack_compute_instance_v2" "loadbalancer" {
+  name        = "haproxy01"
+  image_name  = "${var.IMAGE_NAME}"
+  flavor_name = "${var.INSTANCE_TYPE}"
+  key_pair    = "${openstack_compute_keypair_v2.ssh-keypair.name}"
+  security_groups = ["${openstack_networking_secgroup_v2.local_ssh.name}",
+                     "${openstack_networking_secgroup_v2.any_http.name}"]
+
+  network {
+    name = "${openstack_networking_network_v2.dmz.name}"
+    floating_ip = "${openstack_compute_floatingip_v2.loadbalancer_ip.address}"
+  }
+}
+
+resource "openstack_compute_servergroup_v2" "webservers" {
+  name = "webserver-servergroup"
+  policies = ["anti-affinity"]
+  # policies = ["affinity"]
 }
 
 resource "openstack_compute_instance_v2" "web" {
@@ -66,34 +165,46 @@ resource "openstack_compute_instance_v2" "web" {
   image_name  = "${var.IMAGE_NAME}"
   flavor_name = "${var.INSTANCE_TYPE}"
   key_pair    = "${openstack_compute_keypair_v2.ssh-keypair.name}"
-  security_groups = ["${openstack_networking_secgroup_v2.secgroup_1.name}"]
+  security_groups = ["${openstack_networking_secgroup_v2.local_ssh.name}",
+                     "${openstack_networking_secgroup_v2.local_http.name}"]
 
   network {
-    name = "${openstack_networking_network_v2.network_1.name}"
+    name = "${openstack_networking_network_v2.dmz.name}"
   }
 
-  scheduler_hints = { group = "${openstack_compute_servergroup_v2.test-sg.id}" }
+  scheduler_hints = { group = "${openstack_compute_servergroup_v2.webservers.id}" }
 
   count = 2
 }
 
-resource "openstack_compute_instance_v2" "boot-from-volume" {
-  name            = "boot-from-volume"
+resource "openstack_blockstorage_volume_v1" "database" {
+  region = ""
+  name = "database-volume"
+  description = "data volume for MySQL"
+  size = 50
+}
+
+resource "openstack_compute_instance_v2" "database" {
+  name            = "db01"
   flavor_name     = "${var.INSTANCE_TYPE}"
   key_pair        = "${openstack_compute_keypair_v2.ssh-keypair.name}"
-  security_groups = ["${openstack_networking_secgroup_v2.secgroup_1.name}"]
+  security_groups = ["${openstack_networking_secgroup_v2.local_ssh.name}",
+                     "${openstack_networking_secgroup_v2.local_mysql.name}"]
 
   block_device {
     uuid = "${var.IMAGE_ID}"
     source_type = "image"
-    volume_size = 50
+    volume_size = 30
     boot_index = 0
     destination_type = "volume"
     delete_on_termination = true
   }
 
+  volume {
+    volume_id = "${openstack_blockstorage_volume_v1.database.id}"
+  }
+
   network {
-    name = "${openstack_networking_network_v2.network_1.name}"
+    name = "${openstack_networking_network_v2.dmz.name}"
   }
 }
-
--- terraform-os/variables.tf
+++ terraform-os/variables.tf
@@ -8,3 +8,7 @@ variable "OS_INTERNET_GATEWAY_ID" { default = "1fa82a9d-d87d-4b43-b6dd-54bcbef18
 variable "IMAGE_NAME" { default = "Centos7" }
 variable "IMAGE_ID"   { default =  "4be3ff7d-5e54-4ad4-ba23-af392cdca546" }
 variable "INSTANCE_TYPE" { default = "t1.tiny" }
+
+variable "SSH_KEY"    { default = "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEArrSmhh3oGC/zCorMpOXXppYCRLQMzUpdyQotCuXQ8uD4JHitbReQBT0LuaCtei4xxLcdnpNW7DM/xLfl0yZLWRk0iH5VFjPxvYsgVzhysO8nvG6l2p3RDJOrA2mQROl5yDaAWUE/J2vKezzNJf9f8/JZyuWWafPa89+XTPR3VAuQCzRrCmskW1rpok29IpqM/jM7QCfG20Y7/lLOaCW9UlEAO++WTDQS4X6t6Xf5Lsg6TMGrMuPSQzjUTMGvfhT3dnTuhALLW5bYQirkgAWwSPoHx5yeZZnhH1C0T2ak0Dhx0tlm9sr82DU6x0INWENcdMj95oFlceA5DOqQ/3s3jw==" }
+
+variable "DMZ_Subnet" { default = "192.168.199.0/24" }
